acdf08ba395d1bbe5c0b88a5a6f08080d9678506
Victor <vvvvictor2007@yandex.ru>
2014-04-02 19:32:51 +0400

    Fix gitignore

--- gen/ru/example/PhotoStream/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package ru.example.PhotoStream;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
