fc9ea6bbc815e4bac160e172712634dc059771c6
Victor <vvvvictor2007@yandex.ru>
2014-05-17 20:09:22 +0400

    Fix ActionBar style

--- src/ru/example/PhotoStream/Activities/StreamActivity.java
+++ src/ru/example/PhotoStream/Activities/StreamActivity.java
@@ -134,8 +134,7 @@ public class StreamActivity extends ActionBarActivity implements ActionBar.TabLi
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_activity_actions, menu);
+        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
         return true;
     }
 
